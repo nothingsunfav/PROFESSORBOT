a4e87be10d25005c21c0ce0d36f400ef737ead55
ibhub7 <transformers5720p+18june25op@gmail.com>
2025-06-25 20:46:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,8 +58,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/')
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "") # add multiple channels here, separated by space
 AUTH_CHANNEL = [int(ch) for ch in AUTH_CHANNEL.strip().split()] if AUTH_CHANNEL else []
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '') # use this channel as auth request channel, you need to add this channel in AUTH_CHANNEL variable also
-if AUTH_REQ_CHANNEL:
-    AUTH_REQ_CHANNEL = int(AUTH_REQ_CHANNEL)
+AUTH_REQ_CHANNEL = [int(ch) for ch in AUTH_REQ_CHANNEL.strip().split()] if AUTH_REQ_CHANNEL else []
 
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '100'))
