d30382ea947c721c19b6e41ca10ffe54763f108d
nothingsunfav <nothingsunfav@gmail.com>
2025-09-21 12:32:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,16 +30,16 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://files.catbox.moe/ukiw5m.jpg
 FSUB_IMG = (environ.get('FSUB_IMG', 'https://i.ibb.co/cShkPjcZ/x.jpg')).split() 
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6248675084').split()] 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002583830624').split()]
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002853893090'))  
-BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002583830624'))  
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002583830624'))  
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002690503523')) 
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002947030944').split()]
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003041305681'))  
+BIN_CHANNEL = int(environ.get('BIN_CHANNEL', ''))  
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', ''))  
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '')) 
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002583830624') 
+reqst_channel = environ.get('REQST_CHANNEL_ID', '') 
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002583830624') 
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vihara4360:waldernun@cluster0.xueft5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -50,19 +50,19 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'clustor05')
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_series_tamil_req')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+Jo13VAOePDQ4NzJl')
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Thizz_zoro')
-UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/+Jo13VAOePDQ4NzJl')
+GRP_LNK = environ.get('GRP_LNK', '')
+CHNL_LNK = environ.get('CHNL_LNK', '')
+OWNER_LNK = environ.get('OWNER_LNK', '')
+UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', '')
 
-AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1002883076778") # add multiple channels here, separated by single space
+AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-") # add multiple channels here, separated by single space
 AUTH_CHANNEL = [int(ch) for ch in AUTH_CHANNEL.strip().split()] if AUTH_CHANNEL else []
-AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-1002883076778') # add multiple channels here, separated by single space
+AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '') # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = [int(ch) for ch in AUTH_REQ_CHANNEL.strip().split()] if AUTH_REQ_CHANNEL else []
 
 IS_VERIFY = is_enabled('IS_VERIFY', True)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002853893090'))
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002690503523'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', ''))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', ''))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/+fuhw3bZ97NgxMzE1")
